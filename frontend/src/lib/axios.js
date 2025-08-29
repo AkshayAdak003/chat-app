e2ee170e416195bf656f66d4f67e87e0ca3a1ef3
Akshay Adak <akshayadak3718@gmail.com>
2025-08-29 22:46:39 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-//  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-baseURL: "https://chat-app-3-5anc.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-3-5anc.onrender.com/api",
+//baseURL: "https://chat-app-3-5anc.onrender.com",
   withCredentials: true,
 });
